4cd7e699d6b091ba5d4ba7a566dee47dff873941
Pranaydeep Singh <artemis@vpnq212.ugent.be>
2021-10-07 19:14:34 +0200

    error log for order files

--- verify_order_all.sh
+++ verify_order_all.sh
@@ -1,6 +1,4 @@
-  for file in $(ls data); do 
-    num=$(cat data/$file/order | ./scripts/verify_schedule.py > /dev/null 2>&1; echo $?) 
-    if test $num -ne 0; then 
-      echo -e "$file\t$num"
-    fi
-  done
+for file in $(ls data); do 
+    echo $file
+    cat data/$file/order | ./scripts/verify_schedule.py
+done
